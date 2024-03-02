27bfcbf43789ff3d5b3e6816a2bf0317a60b0e79
Fillipe Drago <fillipedrago@gmail.com>
2024-03-02 11:20:53 -0300

    Adicionado autocommit e código pra criar DB

--- projeto.py
+++ projeto.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+# Host 127.0.0.1 é o local host
 try:
     conn = psycopg2.connect("host=127.0.0.1 port=5433 dbname=postgres user=postgres password=PI314159")
 except psycopg2.Error as e:
@@ -10,4 +11,15 @@ try:
     cur = conn.cursor()
 except psycopg2.Error as e:
     print("Error: Could not get curser to the Database")
+    print(e)
+
+""" Por padrão, todas as ações numa conexão com database através de DB API
+2.0 devem ser commitadas. Setando um autocommit, deixa de haver essa
+necessidade.
+"""
+conn.set_session(autocommit=True)
+
+try:
+    cur.execute("create database myfirstdb")
+except psycopg2.Error as e:
     print(e)
